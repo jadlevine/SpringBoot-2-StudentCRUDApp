97db6ce5066d5873d9f6eeb5e775fde34af79ac7
Josh Levine <levine.joshuaadam@gmail.com>
2023-02-08 16:05:25 -0500

    getmapping endpoint for root setup

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+// this annotation makes this class serve rest endpoints
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
