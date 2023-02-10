3803287d5a0802ce0e4ef3a8668e42843b1713b1
Josh Levine <levine.joshuaadam@gmail.com>
2023-02-10 10:05:26 -0500

    post requests functional, tested with insomnia

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,10 +1,18 @@
 package com.example.demo.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 // annotation below is b/c this is for data access layer
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+  // SQL --> SELECT * FROM student WHERE email = ?
+  // two ways to do it
+  // below, Student is the Student Entity from Student.java
+  @Query("SELECT s FROM Student s Where s.email = ?1")
+  Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,8 +1,7 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +21,16 @@ public class StudentService {
 
   public List<Student> getStudents() {
     return studentRepository.findAll(); // will return a list (this is magic of )
+  }
+
+  public void addNewStudent(Student student) {
+    Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+    if (studentOptional.isPresent()) {
+      throw new IllegalStateException("email taken");
+    }
+    // else if student is NOT present in DB, save student to db (or maybe do some
+    // email validation middleware stuff)
+    studentRepository.save(student);
   }
 
 }
